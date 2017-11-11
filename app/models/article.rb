62a7e034ac07ccb10e7218cd1d71fd39dc0bc26b
Marcello Farigu <marcellofarigu@gmail.com>
2017-11-10 17:15:36 -0800

    show

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
 
-  validates :title, presence: true, length: {minimun: 3, maximum: 50}
-  validates :description, presence: true, length: {minimun: 10, maximum: 500}
+  validates :title, presence: true, length: { in: 3..50 }
+  validates :description, presence: true, length: { in: 10..500 }
 
 end
